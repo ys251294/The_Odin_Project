daab2c6fce707d2c626424bcfd89ddcec26ba591
Yogender Singh <ys251294@gmail.com>
2022-05-08 23:22:12 +0530

    Complete practice Fundamental part - 1

---
+++ assignments/foundations/JS/fundamentals_part-1/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//1
+console.log(23+97);
+
+//2
+console.log(1+2+3+4+5+6);
+
+//3
+console.log((4+6+9)/77);
+
+//4
+
+let a=10;
+console.log(a);
+console.log(9*a);
+// console.log(let b = 7 * a);    // Error let is a reserve keyword;
+// console.log(b);
+
+//5
+let max = 57;
+let actual = max - 13;
+let percentage = actual/max;
+percentage;
